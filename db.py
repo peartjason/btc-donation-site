e0fbba2e13803fa692e957f052f715106158d742
Jason Peart <Jason Peart@Jawrks-JasonP.sequium.com>
2025-06-08 20:25:15 -0500

    📧 Add email logging to DB and custom HTML receipt template

---
+++ db.py
@@ -0,0 +1,8 @@
+
+
+class EmailLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipient = db.Column(db.String(120), nullable=False)
+    subject = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
